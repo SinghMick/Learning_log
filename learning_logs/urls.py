617245d645cdc6693ae5249d2ec8209efd1c2413
Mick Singh <Singh.Mick@outlook.com>
2024-01-12 19:02:08 +1100

    Template Inheritance (parent & child template setup), topic individual page, topic entries view page

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -6,5 +6,8 @@ from .import views
 
 app_name = 'learning_logs'
 urlpatterns = [
-    path('', views.index, name = 'index')
+    path('', views.index, name = 'index'),
+    path('topics/', views.topics, name ='topics'),
+    path ('topics/<int:topic_id>/', views.topic, name = 'topic')
 ]
+
