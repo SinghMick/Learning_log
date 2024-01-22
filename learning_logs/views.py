6dedf7f3310bd6305bbd2e1a3e512ae14ba56880
Mick Singh <Singh.Mick@outlook.com>
2024-01-22 16:15:44 +1100

    Adding manual entry form for topic

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from .models import Topic
+from .forms import TopicForm
 
 
 def index(request):
@@ -18,5 +19,21 @@ def topic(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic' : topic, 'entries' : entries}
-    return render(request, 'learning_logs/topics.html', context)
+    return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """Add new topic"""
+    if request.method != 'POST':
+        #No data submitted, create a blank form
+        form = TopicForm()
+    else:
+        #Post data submitted; process data
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+    
+    #Display a blank or invalid frorm
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
     
