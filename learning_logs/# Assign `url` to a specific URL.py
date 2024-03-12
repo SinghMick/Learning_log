a2b744d6003c683016cc8d0fd7ce99744e08c9c5
Mick Singh <Singh.Mick@outlook.com>
2024-03-12 16:13:34 +1100

    Styling the app using Bootstrap

---
+++ learning_logs/# Assign `url` to a specific URL.py
@@ -0,0 +1,7 @@
+# Assign `url` to a specific URL
+
+url = "https://exampleURL1.com"
+
+# Extract the protocol of `url` along with the syntax following it, display the result
+
+print(url.removeprefix("https://"))
